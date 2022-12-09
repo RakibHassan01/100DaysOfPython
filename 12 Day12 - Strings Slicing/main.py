f2bd70dfef8cac2d295bef00fde2a25509b08d19
Rakib Hassan <rhbappy456@gmail.com>
2022-12-09 18:13:34 +0600

    Day12 - Strings Slicing

---
+++ 12 Day12 - Strings Slicing/main.py
@@ -0,0 +1,20 @@
+names = "Rakib, Viper"
+res = len(names)
+print(res)
+
+
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+# print(fruit[0:4]) # including 0 but not 4
+# print(fruit[1:4]) # including 1 but not 4
+# print(fruit[:5])
+# print(fruit[0:-3])
+# print(fruit[:len(fruit)-3])
+print(fruit[-1:len(fruit) - 3])
+print(fruit[-3:-1])
+
+# Quick Quiz:
+# nm = "Harry"
+# print(nm[-4:-2])
+# @codewithharry
