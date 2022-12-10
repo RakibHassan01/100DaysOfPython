27bd41553bb6b30d5c17c0351c8a67c8541dfab6
Rakib Hassan <rhbappy456@gmail.com>
2022-12-10 18:40:15 +0600

    13 Day13 - String Methods

---
+++ 13 Day13 - String Methods/main.py
@@ -0,0 +1,51 @@
+# String are immutable
+
+a = "Rakib...."
+# print(len(a))
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# str2 = "Silver Spoon"
+# print(str2.split(" "))      #Splits the string at the whitespace " ".
+# print(a.rstrip('.'))
+# str3 = "Hello !!!"
+# print(str3.rstrip("!"))
+# print(a.replace('Rakib....', 'harry,,,,.'))
+# blogHeading = 'introduction tO jAvaScript'
+# print(blogHeading.capitalize())
+
+str1 = "Welcome to the Console!!!"
+print(str1.center(50))
+
+str1 = "He's name is Dan. He is an honest man."
+print(str1.find("ishh"))
+# print(str1.index("ishh"))
+str1 = "WelcomeToTheConsole"
+print(str1.isalnum())
+str1 = "Welcome"
+print(str1.isalpha())
+
+str1 = "hello world"
+print(str1.islower())
+
+str1 = "We wish you a Merry Christmas\n"
+print(str1.isprintable())
+str1 = "         "       #using Spacebar
+print(str1.isspace())
+str2 = "  "       #using Tab
+print(str2.isspace())
+
+str1 = "World Health Organization" 
+print(str1.istitle())
+
+str2 = "To kill a Mocking bird"
+print(str2.istitle())
+
+str1 = "Python is a Interpreted Language" 
+print(str1.startswith("Python"))
+
+str1 = "Python is a Interpreted Language" 
+print(str1.swapcase())
+
+str1 = "His name is Dan. Dan is an honest man."
+print(str1.title())
