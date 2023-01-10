cdb27d7ad23b0caee15259bd75bdc71d99b4bec6
Rakib-Coder00 <rhbappy456@gmail.com>
2023-01-11 00:50:21 +0600

    Day 42 - Enumerate

--- 41 Day41 - Short Hand if else/main.py
+++ 41 Day41 - Short Hand if else/main.py
@@ -4,3 +4,21 @@ print("A") if a > b else print("=") if a == b else print("B")
 
 c = 9 if a > b else 0
 print(c)
+
+
+# Syntax =>
+# result = value_if_true if condition else value_if_false
+
+
+x = 8
+y = 10
+
+max_value = x if x> y else y
+print(max_value)
+
+x = 8
+y = 10
+z = 20
+
+max_value = x if x> y and x> z else y if y > z else z
+print(max_value)
