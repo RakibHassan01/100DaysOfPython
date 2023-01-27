68024d2eeb18d1c4c453fc39bb43894fa4e6f217
Rakib-Coder00 <rhbappy456@gmail.com>
2023-01-27 20:58:12 +0600

    Day 61 - Inheritance in Python

---
+++ 61 Day 61 - Inheritance in Python/main.py
@@ -0,0 +1,18 @@
+class Employee:
+    def __init__(self, name, id) -> None:
+        self.name = name
+        self.id = id
+
+    def showDetails(self):
+        print(f'The name of Employee: {self.id} is {self.name}')
+
+
+class Programmer(Employee):
+    def showLanguage(self):
+        print('The default Language is Python')
+
+
+e1 = Employee('RHB Boss', 420)
+e2 = Programmer('RHB Coder', 420)
+e1.showDetails()
+e2.showLanguage()
